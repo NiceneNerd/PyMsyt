51e685daf8d71f56d5ebaaa1b9923c13f117480d
Caleb Smith <calebdixonsmith@gmail.com>
2019-09-18 22:11:43 -0500

    Removed encoding option, better temp files, debug mode

--- pymsyt/__init__.py
+++ pymsyt/__init__.py
@@ -10,6 +10,7 @@ import zlib
 import yaml
 
 EXEC_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
+_debug = False
 
 if os.name == 'posix':
     MSYT_PATH = EXEC_DIR / 'bin' / 'msyt'
@@ -26,18 +27,21 @@ def parse_msbt(file: Union[str, Path]) -> {}:
     """
     if isinstance(file, str):
         file = Path(file)
-    tmp = Path(tempfile.gettempdir()) / str(hex(zlib.crc32(file.name.encode())) + '.msyt')
+    temp_file = tempfile.NamedTemporaryFile(suffix='.msyt', delete=False)
+    tmp = Path(temp_file.name)
+    temp_file.close()
     if tmp.exists():
         tmp.unlink()
     if export(file, tmp):
         with tmp.open('r', encoding='utf-8') as tmp_file:
-            return yaml.safe_load(tmp_file)
+            parsed = yaml.safe_load(tmp_file)
+        tmp.unlink()
+        return parsed
     else:
-        return RuntimeError('MSYT failed to convert the MSBT file.')
+        raise RuntimeError('MSYT failed to convert the MSBT file.')
 
 
-def write_msbt(text_data: dict, output_file: Union[str, Path], platform: str = 'wiiu',
-               encoding: str = 'utf-16'):
+def write_msbt(text_data: dict, output_file: Union[str, Path], platform: str = 'wiiu'):
     """Creates an MSBT file from an MSYT-formatted text data dict.
 
     :param text_data: The text data to write to MSBT
@@ -46,18 +50,18 @@ def write_msbt(text_data: dict, output_file: Union[str, Path], platform: str = '
     :type output_file: Union[str, Path]
     :param platform: The target platform for the MSBT, either 'wiiu' or 'switch', defaults to 'wiiu'
     :type platform: str, optional
-    :param encoding: The text encoding for the MSBT, defaults to 'utf-16'
-    :type encoding: str, optional
     :raises RuntimeError: Raises a `RuntimeError` if for any reason MSYT fails to create the MSBT
     """
     if isinstance(output_file, str):
         output_file = Path(output_file)
-    tmp = Path(tempfile.gettempdir()) / str(hex(zlib.crc32(output_file.name.encode())) + '.msyt')
-    if tmp.exists():
+    temp_file = tempfile.NamedTemporaryFile(suffix='.msyt', delete=False)
+    tmp = Path(temp_file.name)
+    temp_file.close()
+    with tmp.open('w', encoding='utf-8') as tmp_file:
+        yaml.safe_dump(text_data, tmp_file, allow_unicode=True, encoding='utf-8')
+    if create(tmp, output_file, platform=platform):
         tmp.unlink()
-    with tmp.open('w', encoding=encoding) as tmp_file:
-        yaml.safe_dump(text_data, tmp_file, allow_unicode=True, encoding=encoding)
-    if not create(tmp, output_file, encoding=encoding, platform=platform):
+    else:
         raise RuntimeError('MSYT was unable to create an MSBT from the given text data.')
 
 
@@ -88,13 +92,15 @@ def export(msbt_input: Union[str, Path], msyt_output: Union[str, Path]) -> bool:
     if msbt_input.is_dir():
         args.insert(2, '-d')
     subprocess.call(
-        args, creationflags=subprocess.CREATE_NO_WINDOW,
-        stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL
+        args,
+        creationflags=subprocess.CREATE_NO_WINDOW if not _debug else 0,
+        stderr=subprocess.DEVNULL if not _debug else sys.__stdout__,
+        stdout=subprocess.DEVNULL if not _debug else sys.__stdout__
     )
     return msyt_output.exists()
 
 def create(msyt_input: Union[str, Path], msbt_ouput: Union[str, Path],
-           platform: str = 'wiiu', encoding: str = 'utf16') -> bool:
+           platform: str = 'wiiu') -> bool:
     """Creates MSBT files from MSYT files
 
     :param msyt_input: The MSYT source path, can be either a single MSYT file or a directory
@@ -105,8 +111,6 @@ def create(msyt_input: Union[str, Path], msbt_ouput: Union[str, Path],
     :type msbt_ouput: Union[str, Path]
     :param platform: The target platform for the MSBT, either 'wiiu' or 'switch', defaults to 'wiiu'
     :type platform: str, optional
-    :param encoding: The text encoding for the MSBT, defaults to 'utf-16'
-    :type encoding: str, optional
     :return: Returns True if the output is created successfully, otherwise False
     :rtype: bool
     """
@@ -116,15 +120,6 @@ def create(msyt_input: Union[str, Path], msbt_ouput: Union[str, Path],
         msbt_ouput = Path(msbt_ouput)
     if platform not in ['wiiu', 'switch']:
         raise ValueError(f'Invalid platform "{platform}". Only "wiiu" and "switch" are supported.')
-    if encoding not in ['utf8', 'utf16']:
-        if encoding == 'utf-8':
-            encoding = 'utf8'
-        elif encoding == 'utf-16':
-            encoding = 'utf16'
-        else:
-            raise ValueError(
-                f'Invalid encoding "{platform}". Only "utf8" and "utf16" are supported.'
-            )
 
     msbt_ouput.parent.mkdir(parents=True, exist_ok=True)
     args = [
@@ -133,16 +128,16 @@ def create(msyt_input: Union[str, Path], msbt_ouput: Union[str, Path],
         str(msyt_input.resolve()),
         '-p',
         platform,
-        '-E',
-        encoding,
         '-o',
         str(msbt_ouput.with_suffix('').resolve())
     ]
     if msyt_input.is_dir():
         args.insert(2, '-d')
     subprocess.call(
-        args, creationflags=subprocess.CREATE_NO_WINDOW,
-        stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL
+        args,
+        creationflags=subprocess.CREATE_NO_WINDOW if not _debug else 0,
+        stderr=subprocess.DEVNULL if not _debug else sys.__stdout__,
+        stdout=subprocess.DEVNULL if not _debug else sys.__stdout__
     )
     if msbt_ouput.is_file() and msbt_ouput.with_suffix('').exists():
         shutil.rmtree(msbt_ouput.with_suffix(''))
